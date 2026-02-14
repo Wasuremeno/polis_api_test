a57998b0fe5b7fd1674c52625aca8026aafbc759
Wasuremeno <wasuremono19@gmail.com>
2026-02-14 14:34:02 +0300

    feat: add Article and Comment TypeScript types

--- resources/js/types/index.ts
+++ resources/js/types/index.ts
@@ -1,1 +1,18 @@
 export type * from './auth';
+
+
+export interface Comment {
+    id: number;
+    article_id: number;
+    author_name: string;
+    content: string;
+    created_at: string;
+}
+
+export interface Article {
+    id: number;
+    title: string;
+    content: string;
+    created_at: string;
+    comments?: Comment[];
+}
