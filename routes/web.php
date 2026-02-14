c635ac0fd5e239fe98fdf72d8a682c36287fe3ef
Wasuremeno <wasuremono19@gmail.com>
2026-02-14 14:33:00 +0300

    feat: add Inertia article routes

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('welcome');
-})->name('home');
+use App\Http\Controllers\ArticleController;
+
+Route::get('/', fn() => redirect('/articles'));
+
+Route::get('/articles', [ArticleController::class, 'index']);
+
+Route::get('/articles/create', [ArticleController::class, 'create']);
+
+Route::get('/articles/{article}', [ArticleController::class, 'show']);
