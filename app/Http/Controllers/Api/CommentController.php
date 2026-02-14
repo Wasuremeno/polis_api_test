33ef2bde18df4c85356688e96a158a75aef65b8d
Wasuremeno <wasuremono19@gmail.com>
2026-02-14 14:29:24 +0300

    feat: add Comment API controller store method

--- app/Http/Controllers/Api/CommentController.php
+++ app/Http/Controllers/Api/CommentController.php
@@ -2,10 +2,29 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
+use App\Models\Article;
+use App\Models\Comment;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class CommentController extends Controller
 {
-    //
+    public function store(Request $request, Article $article)
+    {
+        $comment = Comment::create([
+
+            'article_id' => $article->id,
+
+            'author_name' => $request->validate([
+                'author_name' => 'required|string|max:255'
+            ])['author_name'],
+
+            'content' => $request->validate([
+                'content' => 'required|string'
+            ])['content']
+
+        ]);
+
+        return response()->json($comment, 201);
+    }
 }
