638e54fb1372ea417b723599e599c62a0ae51355
Wasuremeno <wasuremono19@gmail.com>
2026-02-14 14:22:20 +0300

    feat: add Comment model with article relation

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -3,8 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Comment extends Model
 {
-    //
+    protected $fillable =
+    [
+        'article_id',
+        'author_name',
+        'content'
+    ];
+
+    public function article(): BelongsTo
+    {
+        return $this->belongsTo(Article::class);
+    }
 }
